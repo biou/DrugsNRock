dc595903862a78c33efc6f7eae067a84bbbb2886
noliv <noliv@collyre.net>
2013-01-26 14:41:17 +0100

    delta partition et musique expérimental

--- GGJ13/GGJ13/WHGameLayer.mm
+++ GGJ13/GGJ13/WHGameLayer.mm
@@ -99,7 +99,7 @@
 -(void) tick: (ccTime) dt
 {
     _elapsedTime+=dt;
-    while ([self.partition nextItemTimestamp] != 0.0 && _elapsedTime > [self.partition nextItemTimestamp]) {
+    while ([self.partition nextItemTimestamp] != 0.0 && _elapsedTime > [self.partition nextItemTimestamp] - 4.9f) {
         NSLog(@"New Item");
         [self newItem:ItemTypeNormal atLane:[self.partition itemLane]];
         [self.partition goToNextItem];
@@ -108,9 +108,8 @@
 
 if ([self.activeItems count]>0){
     WHItem *item = (WHItem *)[self.activeItems objectAtIndex:0];
-    NSLog(@"%f",item.position.y);
-    if(item.position.y >HIT_Y-1 && item.position.y <HIT_Y+1) {
-        NSLog(@"First point -- y:%f temps:%f",item.position.y,_elapsedTime);
+    if(item.position.y >HIT_Y-3 && item.position.y <HIT_Y+3) {
+        NSLog(@"########## First point -- y:%f temps:%f",item.position.y,_elapsedTime);
     }
 }
 }
